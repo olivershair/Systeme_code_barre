5116ac244b880a241aa3f4341ddddbc3eac745bd
YannickNoah <d.assontia@digitalcapfx.com>
2026-03-20 09:38:50 +0100

    Configure ALLOWED_HOSTS for Render deployment

--- barcode_project/settings.py
+++ barcode_project/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-change-this-in-production'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['systeme-code-barre.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
